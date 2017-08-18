b8ba96d6adf707a68ed3f5fa59e8f46d6efcddb8
LAPTOP-F9VC128P\jota9 <jmcarrascoo>
2017-08-18 17:05:51 -0500

    Cambios en la clase del 18/08/2017

--- Codigos/Macros_spss/correr.bat
+++
@@ -1,4 +0,0 @@
-:: la ruta en donde esta instalado SPSS
-cd C:\\Program Files\\IBM\\SPSS\\Statistics\\22
-:: la ruta del archivo de la macro
-stats.exe -production "C:\\Users\\jota9\\Documents\\SPSS_MACROS\\StatisticsJob4.spj"
